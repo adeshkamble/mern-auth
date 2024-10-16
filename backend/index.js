e067b6a618a9d04feaae9aeeb1fd3572caee3b30
Adesh <adeshkamble75@gmail.com>
2024-10-16 19:17:19 +0530

    Reset Password Endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Use Express's built-in JSON middleware to parse incoming JSON requests
 app.use(express.json());
+app.use(cookieParser());
 
 // Mount the authentication routes under the /api/auth path
 app.use("/api/auth", authRoutes);
